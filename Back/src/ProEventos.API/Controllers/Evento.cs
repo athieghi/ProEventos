96370ddb70d7c3e9c7562d4e16d9c5be8ca127d5
Anderson Renato Thieghi de Souza <anderson.thieghi@linx.com.br>
2022-10-11 14:27:55 -0300

    first commit

---
+++ Back/src/ProEventos.API/Controllers/Evento.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[] {
+                new Evento() {
+                EventoId = 1,
+                Tema = "Angular 11 e Net 5",
+                Local = "Belo Horizonte",
+                Lote = "1 Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImagemURL = "foto.png"
+                },
+
+                new Evento() {
+                EventoId = 2,
+                Tema = "Python",
+                Local = "São Paulo",
+                Lote = "1 Lote",
+                QtdPessoas = 150,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImagemURL = "foto2.png"
+                }
+        };
+
+
+
+        public EventoController()
+        {
+            
+        }
+        
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Ex Post";
+        }
+    }
+}
